9cd0812785123120d5da06f8e6b6305243804247
kebi-be <134584807+kebi-be@users.noreply.github.com>
2025-01-24 09:59:06 +0100

    Added package info

--- m2isar/backends/tc_generator/__init__.py
+++ m2isar/backends/tc_generator/__init__.py
@@ -4,6 +4,7 @@
 ##
 ## Copyright (c) 2025 TUM Department of Electrical and Computer Engineering.
 ## Copyright (c) 2025 DLR-SE Department of System Evolution and Operation
+"""Backend for automatic generation of testcases for custom instructions"""
 
 import pathlib
 
